aed16f79e80ea66737f56a1ac0b90da2395ffde9
mcgheej <mcghee.j@btinternet.com>
2025-08-06 16:06:43 +0100

    Add Angular Material

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,14 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'ck-root',
-  imports: [RouterOutlet],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('cookster');
-  temp = '123456789003242343243243243243243432432432432432432434324234324324324324322';
 }
---
+++ src/docs/dev-diary/20250806-02-add-angular-material.md
@@ -0,0 +1,9 @@
+# Add Angular Material
+
+To add Angular Material simply follow instruction available online.
+
+```bat
+> ng add @angular/material
+```
+
+The install asked for a theme name so selected _azure-blue_ as a starter but then played with the `src/custom-theme.scss` contents to try different themes. Finally added an example toolbar and styled it using mixins to use primary colour (azure) rather than transparent. Also set the icons to be white rather than black.
