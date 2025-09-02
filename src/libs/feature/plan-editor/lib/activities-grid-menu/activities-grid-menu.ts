43876b7d82745deb71e1304704fd4e9cd95f6dc2
mcgheej <mcghee.j@btinternet.com>
2025-09-02 17:23:43 +0100

    Added grid menu with support for changing time zoom scale

--- src/libs/feature/plan-editor/lib/activities-grid-menu/activities-grid-menu.ts
+++ src/libs/feature/plan-editor/lib/activities-grid-menu/activities-grid-menu.ts
@@ -1,21 +1,32 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { ACTIVITIES_GRID_HEADER_HEIGHT, DEFAULT_TOOLTIP_SHOW_DELAY } from '@util/app-config/lib/constants';
+import { PlanEditorDataService } from '../plan-editor-data-service';
 
 @Component({
   selector: 'ck-activities-grid-menu',
-  imports: [MatButtonModule, MatIconModule, MatTooltipModule],
+  imports: [MatButtonModule, MatIconModule, MatMenuModule, MatTooltipModule],
   templateUrl: './activities-grid-menu.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ActivitiesGridMenu {
-  protected tooltipShowDelay = DEFAULT_TOOLTIP_SHOW_DELAY;
+  private readonly editorData = inject(PlanEditorDataService);
 
+  protected tooltipShowDelay = DEFAULT_TOOLTIP_SHOW_DELAY;
   protected readonly headerHeight = `h-[${ACTIVITIES_GRID_HEADER_HEIGHT}px]` as const;
 
-  openTimeColumnMenu() {
-    console.log('Open time column menu');
+  protected readonly gridMenuData = computed(() => {
+    return {
+      pixelsPerHour: this.editorData.activitiesGridPixelsPerHour(),
+    };
+  });
+
+  setPixelsPerHour(pixelsPerHour: number) {
+    if (pixelsPerHour !== this.editorData.activitiesGridPixelsPerHour()) {
+      this.editorData.setActivitiesGridPixelsPerHour(pixelsPerHour);
+    }
   }
 }
