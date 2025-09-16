917e7ce69a0a2460b2a9987417fd068a1d2276f1
mcgheej <mcghee.j@btinternet.com>
2025-09-16 18:26:37 +0100

    Use tooltip to show activity start and end times when mouse over title

--- src/libs/feature/plan-editor/lib/activities-grid/resource-lanes-layer/lane-column/activity-tile/activity-tile.ts
+++ src/libs/feature/plan-editor/lib/activities-grid/resource-lanes-layer/lane-column/activity-tile/activity-tile.ts
@@ -1,14 +1,30 @@
 import { NgStyle } from '@angular/common';
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { DEFAULT_TOOLTIP_SHOW_DELAY } from '@util/app-config/lib/constants';
 import { DisplayTile } from '@util/data-types/index';
 
 @Component({
   selector: 'ck-activity-tile',
-  imports: [NgStyle, MatIconModule],
+  imports: [NgStyle, MatIconModule, MatTooltipModule],
   templateUrl: './activity-tile.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ActivityTile {
   tile = input.required<DisplayTile>();
+
+  protected startEndTimes = computed(() => {
+    const startTime = this.timeFromMidnightToString(this.tile().startMinsFromMidnight);
+    const endTime = this.timeFromMidnightToString(this.tile().endMinsFromMidnight);
+    return this.tile().activity.name + ': ' + startTime + ' - ' + endTime;
+  });
+
+  protected readonly showDelay = DEFAULT_TOOLTIP_SHOW_DELAY;
+
+  private timeFromMidnightToString(minsFromMidnight: number): string {
+    const hours = Math.floor(minsFromMidnight / 60);
+    const minutes = minsFromMidnight % 60;
+    return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
+  }
 }
