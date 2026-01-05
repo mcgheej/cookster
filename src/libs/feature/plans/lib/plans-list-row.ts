d552d009286328fe65bf31a14dd9a7269e6bf49e
mcgheej <mcghee.j@btinternet.com>
2026-01-05 16:32:28 +0000

    Show plan colour in plan detail in plan feature

--- src/libs/feature/plans/lib/plans-list-row.ts
+++ src/libs/feature/plans/lib/plans-list-row.ts
@@ -1,7 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, input, output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, input, output } from '@angular/core';
 import { PlanDetail } from './plan-detail';
 import { PlanSummary } from '@util/data-types/index';
+import { opaqueColor } from '@util/color-utilities/index';
+import { DEFAULT_COLOR_OPACITY, DEFAULT_PLAN_COLOR, googleColors } from '@util/app-config/index';
 
 @Component({
   selector: 'ck-plans-list-row',
@@ -10,7 +12,14 @@ import { PlanSummary } from '@util/data-types/index';
     <div class="w-full cursor-pointer text-sm" (click)="onToggleExpand($event)">
       <div class="grid grid-cols-[1fr_250px] grid-rows-[40px]">
         <div class="row-start-1 col-start-1 row-end-3 col-end-2 flex items-center justify-start pl-1 select-none">
-          {{ planSummary().name }}
+          <div>
+            @if (expandedPlanId() === planSummary().id) {
+              <div class="inline-block size-[10px] rounded-[50%]" [style.backgroundColor]="planColor()"></div>
+            } @else {
+              <div class="inline-block size-[10px]"></div>
+            }
+            <div class="inline-block pl-1 select-none">{{ planSummary().name }}</div>
+          </div>
         </div>
         <div class="row-start-1 col-start-2 row-end-3 col-end-2 pl-1 flex items-center justify-end">
           {{ planSummary().dateTime | date: 'd LLL yyyy, HH:mm' }}
@@ -43,4 +52,9 @@ export class PlansListRow {
     ev.stopPropagation();
     this.toggleExpand.emit();
   }
+
+  protected readonly planColor = computed(() => {
+    const color = googleColors[this.planSummary().color].color ?? DEFAULT_PLAN_COLOR;
+    return opaqueColor(color, DEFAULT_COLOR_OPACITY);
+  });
 }
