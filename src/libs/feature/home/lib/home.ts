018c401c230f6a2651a6d9b96114ff0ce3a4d42b
mcgheej <mcghee.j@btinternet.com>
2026-02-11 16:26:35 +0000

    deployment remnants

--- src/libs/feature/home/lib/home.ts
+++ src/libs/feature/home/lib/home.ts
@@ -2,7 +2,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'ck-home',
-  template: ` <div>Home page under construction - v1.0.7</div> `,
+  template: ` <div>Home page under construction - v1.1.0</div> `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Home {}
