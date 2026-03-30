d3536e55a8c763641196483de0d94abb1bc9f65d
mcgheej <mcghee.j@btinternet.com>
2026-03-30 15:31:52 +0100

    Updated version number

--- src/libs/feature/home/lib/home.ts
+++ src/libs/feature/home/lib/home.ts
@@ -2,7 +2,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'ck-home',
-  template: ` <div>Home page under construction - v1.1.0</div> `,
+  template: ` <div>Home page under construction - v1.2.0</div> `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Home {}
